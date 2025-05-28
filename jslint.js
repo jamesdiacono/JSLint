6a128ca44ba12d304e998992dd35a18f4b097a42
James Diacono <james@diacono.com.au>
2025-05-28 10:46:11 +1000

    [].reduce

--- jslint.js
+++ jslint.js
@@ -1,5 +1,5 @@
 // jslint.js
-// 2025-05-13
+// 2025-05-28
 // Copyright (c) 2015 Douglas Crockford  (www.JSLint.com)
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -2628,6 +2628,7 @@ infix(".", 170, function (left) {
                 && name.id !== "forEach"
                 && name.id !== "join"
                 && name.id !== "map"
+                && name.id !== "reduce"
             )
         )
         && (left.id !== "+" || name.id !== "slice")
@@ -4850,7 +4851,7 @@ export default Object.freeze(function jslint(
     }
     return {
         directives,
-        edition: "2025-05-13",
+        edition: "2025-05-28",
         exports,
         froms,
         global,
